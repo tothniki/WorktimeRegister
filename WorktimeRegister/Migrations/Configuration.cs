386d489113780783862b2ad02fb26e57ffb1d60a
Tóth Nikoletta <tothniki.ntt@gmail.com>
2017-04-14 17:26:59 +0200

    Seed Membership (admin user in admin role)

--- WorktimeRegister/Migrations/Configuration.cs
+++ WorktimeRegister/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace WorktimeRegister.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Security;
+    using WebMatrix.WebData;
     using WorktimeRegister.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WorktimeRegister.Models.WorktimeRegisterDb>
@@ -44,6 +46,32 @@ namespace WorktimeRegister.Migrations
                  Leaving = DateTime.Now.AddHours(6),
                  Email = "nagy.gonzi@gmail.com"
              });
+
+            //Seed user and admin
+            SeedMembership();
+        }
+
+        private void SeedMembership()
+        {
+            //just to make sure that everything is set up
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+            
+            //get access to a current role provider
+            var roles = (SimpleRoleProvider)Roles.Provider;
+            var membership = (SimpleMembershipProvider)Membership.Provider;
+
+            if (!roles.RoleExists("Admin"))
+            {
+                roles.CreateRole("Admin");
+            }
+            if(membership.GetUser("adminUser", false) == null)
+            {
+                membership.CreateUserAndAccount("adminUser", "adminUser123");
+            }
+            if (!roles.GetRolesForUser("adminUser").Contains("Admin"))
+            {
+                roles.AddUsersToRoles(new[] { "adminUser" }, new[] { "Admin" });
+            }
         }
     }
 }
