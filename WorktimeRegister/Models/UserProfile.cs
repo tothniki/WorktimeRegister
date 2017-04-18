a7691c804a07e23c92a50958cd0b02d6de39643a
Tóth Nikoletta <tothniki.ntt@gmail.com>
2017-04-18 15:34:53 +0200

    UserInfo labels, set Username not editable

--- WorktimeRegister/Models/UserProfile.cs
+++ WorktimeRegister/Models/UserProfile.cs
@@ -13,10 +13,20 @@ namespace WorktimeRegister.Models
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+
+        [Display(Name = "User name")]
         public string UserName { get; set; }
+
+        [Display(Name = "Email adress")]
         public string Email { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
     }
 }
