5a0bf6353d62ace27533830f7397b2d0533badc5
Tóth Nikoletta <tothniki.ntt@gmail.com>
2017-04-26 19:00:06 +0200

    Admin user listed function with DropDownList implemented

--- WorktimeRegister/Controllers/AdminController.cs
+++ WorktimeRegister/Controllers/AdminController.cs
@@ -3,18 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WorktimeRegister.Models;
 
 namespace WorktimeRegister.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        WorktimeRegisterDb _db = new WorktimeRegisterDb();
         //
         // GET: /Admin/
 
         public ActionResult Index()
         {
-            return View();
+            return RedirectToRoute(new { controller = "Home", action = "Index" });
+        }
+
+        //
+        // GET: /Admin/Worktime
+
+        public ActionResult Worktime()
+        {
+            var users = _db.UserProfiles.OrderBy(r => r.UserName)
+                            .Select(r => r).ToList();
+            return View(users);
+        }
+
+        //
+        // GET: /Admin/Users
+
+        public ActionResult Users()
+        {
+            string uName = "";
+            if (Request.Form["SelectUser"] != null)
+            {
+                uName = Request.Form["SelectUser"].ToString();
+                ViewBag.User = uName;
+            }
+                var users = _db.UserProfiles.OrderBy(r => r.UserName)
+                            .Select(r => r);
+            
+                return View(users);
         }
 
         //
@@ -101,6 +130,15 @@ namespace WorktimeRegister.Controllers
             {
                 return View();
             }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
