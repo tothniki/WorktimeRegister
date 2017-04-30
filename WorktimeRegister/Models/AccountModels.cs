012e9305db921812459f6f972939256a617471f6
Tóth Nikoletta <tothniki.ntt@gmail.com>
2017-04-30 02:45:54 +0200

    Forgot / Reset Password function implemented and working

--- WorktimeRegister/Models/AccountModels.cs
+++ WorktimeRegister/Models/AccountModels.cs
@@ -69,6 +69,11 @@ namespace WorktimeRegister.Models
         [Display(Name = "Email adress")]
         public string Email { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm Email")]
+        [Compare("Email", ErrorMessage = "The emails you've entered do not match.")]
+        public string ConfirmEmail { get; set; }
+
         [Required(ErrorMessage = "{0} is required")]
         [StringLength(20, ErrorMessage = "The {0} must be maximum {1} characters long.")]
         [Display(Name = "User name")]
@@ -97,4 +102,31 @@ namespace WorktimeRegister.Models
         public string ProviderDisplayName { get; set; }
         public string ProviderUserId { get; set; }
     }
+
+    //For the password reset:
+    public class ForgotPasswordModel
+    {
+        [Required(ErrorMessage = "We need your email to send you a reset link!")]
+        [Display(Name = "Your account email")]
+        [EmailAddress(ErrorMessage = "Not a valid email--what are you trying to do here?")]
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        [Required]
+        [Display(Name = "New Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "New password and confirmation does not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ReturnToken { get; set; }
+    }
+
 }
