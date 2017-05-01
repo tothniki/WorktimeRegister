eae2eca942ffef67c53cbb8bd9d6d3731f5842e4
Tóth Nikoletta <tothniki.ntt@gmail.com>
2017-05-01 21:46:43 +0200

    Making format and change in export dropdownlist not have label value

--- WorktimeRegister/Classes/CustomExcelHelper.cs
+++ WorktimeRegister/Classes/CustomExcelHelper.cs
@@ -53,6 +53,8 @@ namespace WorktimeRegister.Classes
                         //Set user details
                         SetUserDetails(ws, user, userFullName);
 
+                        SetCompanyDetails(ws);
+
                         //Set worktime Table Headers
                         SetWorktimeTableHeader(ws);
 
@@ -68,10 +70,11 @@ namespace WorktimeRegister.Classes
             return pck;
         }
 
+
         private void SetPageHeader(ExcelWorksheet ws)
         {
-            ws.Cells[1, 1].Value = "Worktime sheet";
-            ws.Cells[1, 2].Value = Year + "-" + Month;
+            ws.Cells["A1:B1"].Merge = true;
+            ws.Cells[1, 1].Value = "Worktime sheet " + Year + "-" + Month;
         }
 
         private void SetUserDetails(ExcelWorksheet ws, UserProfile user, string fullName)
@@ -81,8 +84,8 @@ namespace WorktimeRegister.Classes
             ws.Cells[4, 2].Value = user.DateOfBirth;
             ws.Cells[5, 1].Value = "Phone number:";
             ws.Cells[5, 2].Value = user.PhoneNumber;
-            ws.Cells[6, 1].Value = "Email:";
-            ws.Cells[6, 2].Value = user.Email;
+            ws.Cells["A6:B6"].Merge = true;
+            ws.Cells[6, 1].Value = "Email: " + user.Email;
         }
 
         private void SetWorktimeTableHeader(ExcelWorksheet ws)
@@ -124,34 +127,30 @@ namespace WorktimeRegister.Classes
                 }
             }
             row += 1;
-            ws.Cells[row, 3].Value = "Sum hours:";
-            ws.Cells[row, 4].Value = sumHoursADay;
+            ws.Cells[row +1, 3].Value = "Sum hours:";
+            ws.Cells[row +1, 4].Value = sumHoursADay;
         }
 
         private void FormatWorksheet(ExcelWorksheet ws, int row)
         {
-            //
-            // Format the excel content
-            using (ExcelRange rng = ws.Cells["A1:D6"])
+            //Format Page Header
+            using (ExcelRange rng = ws.Cells[ "A1:E1"])
             {
                 rng.Style.Font.Bold = true;
                 rng.Style.Fill.PatternType = ExcelFillStyle.Solid; //Set Pattern for the background to Solid 
-                rng.Style.Fill.BackgroundColor.SetColor(Color.LightBlue); //Set color to DarkGray 
+                rng.Style.Fill.BackgroundColor.SetColor(Color.LightSlateGray); //Set color to DarkGray 
                 rng.Style.Font.Color.SetColor(Color.Black);
             }
-            //Format Date col
-            using (ExcelRange rng = ws.Cells[9, 1, row - 1, 1])
-            {
-                rng.Style.Numberformat.Format = "yyyy.mm.dd";
-                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
 
-            }
-            //Format Arrival, Leaving cols
-            using (ExcelRange rng = ws.Cells[9, 2, row - 1, 3])
+            //
+            // Format User infos
+            using (ExcelRange rng = ws.Cells["A3:B6"])
             {
-                rng.Style.Numberformat.Format = "hh:mm:ss";
+                rng.Style.Font.Bold = true;
+                rng.Style.Fill.PatternType = ExcelFillStyle.Solid; //Set Pattern for the background to Solid 
+                rng.Style.Fill.BackgroundColor.SetColor(Color.LightSlateGray); //Set color to DarkGray 
+                rng.Style.Font.Color.SetColor(Color.Black);
                 rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-
             }
 
             //Format Date Of Birth
@@ -160,9 +159,72 @@ namespace WorktimeRegister.Classes
                 cell.Style.Numberformat.Format = "yyyy.mm.dd";
             }
 
+            //
+            // Format Company infos
+            using (ExcelRange rng = ws.Cells["D3:E6"])
+            {
+                rng.Style.Font.Bold = true;
+                rng.Style.Fill.PatternType = ExcelFillStyle.Solid; //Set Pattern for the background to Solid 
+                rng.Style.Fill.BackgroundColor.SetColor(Color.LightSlateGray); //Set color to DarkGray 
+                rng.Style.Font.Color.SetColor(Color.Black);
+                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+            }
+
+            //Format table header
+            using (ExcelRange rng = ws.Cells["A8:D8"])
+            {
+                rng.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+            }
+
+            //Format table border part
+            using (ExcelRange rng = ws.Cells["B8:C8"])
+            {
+                rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                rng.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+            }
+
+            //Format Date col
+            using (ExcelRange rng = ws.Cells[9, 1, row - 1, 1])
+            {
+                rng.Style.Numberformat.Format = "yyyy.mm.dd";
+                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            }
+
+            
+            //Format Arrival, Leaving cols
+            using (ExcelRange rng = ws.Cells[9, 2, row - 1, 3])
+            {
+                rng.Style.Numberformat.Format = "hh:mm:ss";
+                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                rng.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+
+            }
+
+            //format Sum hours:
+            using (ExcelRange rng = ws.Cells[row+1, 3, row+1 , 4])
+            {
+                rng.Style.Border.BorderAround(ExcelBorderStyle.Thin);
+                rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            }
+
             //Set autofit the cells' size
             ws.Cells.AutoFitColumns();
         }
 
+        private void SetCompanyDetails(ExcelWorksheet ws)
+        {
+            Company company = _db.Company.FirstOrDefault();
+
+            ws.Cells[3, 4].Value = company.Name;
+            ws.Cells[4, 4].Value = "Address:";
+            ws.Cells[4, 5].Value = company.Country + ", " + company.City;
+            ws.Cells[5, 5].Value = company.PostCode + ", " + company.Address;
+            ws.Cells["D6:E6"].Merge = true;
+            ws.Cells[6, 4].Value = "Email: " + company.Email;
+        }
     }
 }
